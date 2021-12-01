c7fef8745348591b7397f060a3955641e20f0106
bbonche <bbonche@henix.fr>
2021-12-01 14:41:35 +0100

    add starting class

---
+++ src/main/java/fr/ans/psc/PscApiMajApplication.java
@@ -0,0 +1,10 @@
+package fr.ans.psc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PscApiMajApplication {
+
+    public static void main(String[] args) { SpringApplication.run(PscApiMajApplication.class, args); }
+}
