e24ddcd7b21620e5c3b6796303d69ef58ed981c7
bbonche <bbonche@henix.fr>
2021-12-01 11:03:42 +0100

    add MongoDB config

---
+++ src/main/java/fr/ans/psc/config/MongoDBConfig.java
@@ -0,0 +1,37 @@
+package fr.ans.psc.config;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Slf4j
+@EnableAutoConfiguration(exclude = {EmbeddedMongoAutoConfiguration.class})
+@Service
+public class MongoDBConfig {
+
+    @Value("${spring.data.mongodb.host}")
+    private String mongoHost;
+
+    @Value("${spring.data.mongodb.port}")
+    private String mongoPort;
+
+    @Value("${spring.data.mongodb.database}")
+    private String mongoDatabase;
+
+    @Bean
+    public MongoTemplate mongoTemplate() {
+        MongoClient mongoClient = MongoClients.create("mongodb://" + mongoHost + ":" + mongoPort);
+        log.info("création d'une connexion à MongoDB avec   mongodb://" + mongoHost + ":" + mongoPort
+                + " mongoDatabase: " + mongoDatabase);
+
+        return new MongoTemplate(mongoClient, mongoDatabase);
+    }
+}
