0a60609df25abd32b2256243b1ad25e3b6e446fa
bbonche <bbonche@henix.fr>
2021-12-06 20:32:20 +0100

    clean test class

--- src/test/java/fr/ans/psc/pscapimajv2/api/ps/PsOperationTest.java
+++ src/test/java/fr/ans/psc/pscapimajv2/api/ps/PsOperationTest.java
@@ -4,89 +4,51 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import de.bwaldvogel.mongo.MongoServer;
+import com.jupiter.tools.spring.test.mongo.annotation.MongoDataSet;
+import com.jupiter.tools.spring.test.mongo.junit5.MongoDbExtension;
 import fr.ans.psc.PscApiMajApplication;
-import fr.ans.psc.api.PsApiDelegate;
 import fr.ans.psc.model.Ps;
-import fr.ans.psc.model.PsRef;
-import fr.ans.psc.pscapimajv2.EnableMongoTestServer;
-import fr.ans.psc.pscapimajv2.MongoTestServerConfiguration;
-import fr.ans.psc.repository.PsRefRepository;
-import fr.ans.psc.repository.PsRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.AutoConfigureDataMongo;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.List;
-
-@ExtendWith({RestDocumentationExtension.class, SpringExtension.class}) // pour restdocs
-@SpringBootTest(classes = PsOperationTest.TestConfiguration.class) // (properties = "file.encoding=UTF-8")
+@ExtendWith({RestDocumentationExtension.class, SpringExtension.class, MongoDbExtension.class}) // pour restdocs
+@SpringBootTest
 @AutoConfigureMockMvc
-@ContextConfiguration(classes = {PscApiMajApplication.class, MongoTestServerConfiguration.class})
+@AutoConfigureDataMongo
+@ContextConfiguration(classes = PscApiMajApplication.class)
+@DirtiesContext
+@ActiveProfiles("test")
 public class PsOperationTest {
 
-    /**
-     * The mock mvc.
-     */
     @Autowired
     private MockMvc mockMvc;
-
-    @Autowired
-    private PsApiDelegate psApiDelegate;
-
-    @Autowired
-    private PsRepository psRepository;
-    @Autowired
-    private PsRefRepository psRefRepository;
-    @Autowired
-    private MongoServer mongoServer;
     @Autowired
     private MongoTemplate mongoTemplateTest;
     @Autowired
     private MongoDatabaseFactory mongoDatabaseFactory;
 
     @BeforeEach
-    public void setUp() throws IOException {
-//        psRepository.deleteAll();
-//        psRefRepository.deleteAll();
-        mongoTemplateTest.dropCollection("ps");
-        mongoTemplateTest.dropCollection("psref");
-
-        ObjectMapper mapper = new ObjectMapper();
-        List<Ps> psList = mapper.readValue(
-                new File("/C:/WS/dev/ANS/prosanteconnect-platform-projects/psc-api-maj-v2/src/test/resources/PsDataSet.json"),
-                new TypeReference<List<Ps>>() {
-                });
-        psList.forEach(ps -> mongoTemplateTest.save(ps, "ps"));
-        List<PsRef> psRefList = mapper.readValue(
-                new File("/C:/WS/dev/ANS/prosanteconnect-platform-projects/psc-api-maj-v2/src/test/resources/PsRefDataSet.json"),
-                new TypeReference<List<PsRef>>() {
-                });
-        psRefList.forEach(psRef -> mongoTemplateTest.save(psRef, "psref"));
-        mongoTemplateTest.findAll(Ps.class).forEach(ps -> {
-            System.out.println("-------------------------------------STORED PS --------------- ");
-            System.out.println(ps.toString());
-        });
+    public void setUp() {
     }
 
     @Test
+    @MongoDataSet(value = "/dataset/8000000001.json", cleanBefore = true, cleanAfter = true)
     public void getPsById() throws Exception {
 
+        mongoTemplateTest.findAll(Ps.class).forEach(ps -> System.out.println(ps.toString()));
 
         // cas nominal : Ps existe et retourné
         mockMvc.perform(get("/api/v1/ps/800000000001").header("Accept", "application/json"))
@@ -125,12 +87,5 @@ public class PsOperationTest {
 
         // le body de la requête est mal formé
     }
-
-    @Configuration
-    @EnableMongoTestServer
-    @EnableMongoRepositories(basePackages = "fr.ans.psc.repository")
-    protected static class TestConfiguration {
-    }
-
 
 }
